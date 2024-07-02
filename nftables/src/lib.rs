cca6420aedea67747fbd0cacded99ff3a5e74669
John Agapeyev <jagapeyev@gmail.com>
2024-07-01 21:02:21 -0700

    Added dry run toggling capabilities into the wrapped NftCtx type

--- nftables/src/lib.rs
+++ nftables/src/lib.rs
@@ -14,6 +14,13 @@ impl NftCtx {
                 .expect("Could not allocate nft_ctx"),
         }
     }
+
+    pub fn is_dry_run(&self) -> bool {
+        unsafe { nft_ctx_get_dry_run(self.inner.as_ptr()) }
+    }
+    pub fn set_dry_run(&mut self, dry_run: bool) {
+        unsafe { nft_ctx_set_dry_run(self.inner.as_ptr(), dry_run) }
+    }
 }
 
 impl Drop for NftCtx {
@@ -27,7 +34,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(true, true);
+    fn dry_run_toggling() {
+        let mut ctx = NftCtx::new();
+        assert_eq!(ctx.is_dry_run(), false);
+        ctx.set_dry_run(true);
+        assert_eq!(ctx.is_dry_run(), true);
+        ctx.set_dry_run(false);
+        assert_eq!(ctx.is_dry_run(), false);
     }
 }
