876b1cede966eb36dd8926e44c09f40ff08f6143
John Agapeyev <jagapeyev@gmail.com>
2024-07-01 17:00:23 -0700

    Basic axum boilerplate to serve the frontend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use tokio;
+use tower_http::services::{ServeDir, ServeFile};
+
+async fn run_server() {
+    let serve_dir = ServeDir::new("dist");
+    let serve_file = ServeFile::new("dist/index.html");
+
+    let app = Router::new()
+        .route("/api/", get(|| async { "Hello, World!" }))
+        .fallback_service(serve_dir);
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    run_server().await;
 }
