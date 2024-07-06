f472cd04a6cecbcca59a54b911ed2b192d854cc1
John Agapeyev <jagapeyev@gmail.com>
2024-07-05 19:21:23 -0700

    Made a trivial list tables nft JSON api call work

--- nftables/src/lib.rs
+++ nftables/src/lib.rs
@@ -1,6 +1,12 @@
+use std::ffi::{CStr, CString};
 use std::ptr::NonNull;
 
 use nftables_sys::*;
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+pub enum Error {
+    Something,
+}
 
 #[derive(Clone, Debug, Eq, PartialEq, Hash)]
 pub struct NftCtx {
@@ -20,6 +26,22 @@ impl NftCtx {
     }
     pub fn set_dry_run(&mut self, dry_run: bool) {
         unsafe { nft_ctx_set_dry_run(self.inner.as_ptr(), dry_run) }
+    }
+    pub fn set_json(&mut self) {
+        unsafe { nft_ctx_input_set_flags(self.inner.as_ptr(), NFT_CTX_INPUT_JSON) };
+    }
+
+    pub fn run_cmd_bytes(&mut self, cmd: &CStr) -> Result<(), Error> {
+        let ret = unsafe { nft_run_cmd_from_buffer(self.inner.as_ptr(), cmd.as_ptr()) };
+        if ret == 0 {
+            return Ok(());
+        }
+        //TODO: Convert library error into a proper return value
+        unimplemented!();
+    }
+    pub fn run_cmd_str(&mut self, cmd: &str) -> Result<(), Error> {
+        let converted = CString::new(cmd).unwrap();
+        self.run_cmd_bytes(&converted)
     }
 }
 
@@ -41,5 +63,16 @@ mod tests {
         assert_eq!(ctx.is_dry_run(), true);
         ctx.set_dry_run(false);
         assert_eq!(ctx.is_dry_run(), false);
+    }
+
+    #[test]
+    fn basic_command_running() {
+        let mut ctx = NftCtx::new();
+        ctx.set_json();
+        ctx.set_dry_run(true);
+        ctx.run_cmd_str(
+            "{ \"nftables\": [ { \"list\": { \"tables\": { \"family\": \"ip\" } } } ] }",
+        )
+        .unwrap();
     }
 }
