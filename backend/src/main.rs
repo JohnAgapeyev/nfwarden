96f97dce893cdeefbbd44abe0fb669b5912440a4
John Agapeyev <jagapeyev@gmail.com>
2024-07-20 12:28:07 -0700

    Got a trivial list-all-tables API call work on the backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,39 @@
-use axum::{response::Json, routing::get, Router};
+use axum::{response::IntoResponse, response::Json, response::Result, routing::get, Router};
 use serde_json::{json, Value};
 use tokio;
 use tower_http::services::{ServeDir, ServeFile};
 
-async fn get_tables() -> Json<Value> {
-    Json(json!({ "data": 42 }))
+use nftables::*;
+
+async fn get_tables() -> Result<Json<NftOutput>, Json<String>> {
+    let mut ctx = NftCtx::new();
+    ctx.set_json();
+    ctx.set_dry_run(true);
+
+    let input = NftOutput {
+        items: vec![NftObjects::List(NftList {
+            tables: NftTableList {
+                family: None,
+                name: None,
+                handle: None,
+            },
+        })],
+    };
+
+    println!("{}", serde_json::to_string(&input).unwrap());
+
+    //Json(ctx.run_cmd_str(&serde_json::to_string(&input).unwrap()))
+
+    match ctx.run_cmd_str(&serde_json::to_string(&input).unwrap()) {
+        Ok(raw) => {
+            let parsed = serde_json::from_slice::<NftOutput>(raw.as_bytes()).unwrap();
+            println!("Serialized Input JSON Parsed:\n{parsed:#?}");
+            return Ok(Json(parsed));
+        }
+        Err(err_msg) => {
+            return Err(Json(err_msg));
+        }
+    }
 }
 
 fn get_api_v1_router() -> Router {
