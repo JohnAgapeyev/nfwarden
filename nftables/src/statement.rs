01bf37bf3982b9528ce3263d87cc6cec5e24905f
John Agapeyev <jagapeyev@gmail.com>
2024-08-04 19:40:00 -0700

    Nat boilerplate and some test module separation

--- nftables/src/statement.rs
+++ nftables/src/statement.rs
@@ -142,6 +142,100 @@ pub struct DupStatement {
 
 #[derive(Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
+pub enum NatFlag {
+    Random,
+    #[serde(rename = "fully-random")]
+    FullyRandom,
+    Persistent,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
+pub struct SnatStatement {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub addr: Option<Expression>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub family: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub port: Option<Expression>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub flags: Option<Vec<NatFlag>>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
+pub struct DnatStatement {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub addr: Option<Expression>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub family: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub port: Option<Expression>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub flags: Option<Vec<NatFlag>>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
+pub struct MasqueradeStatement {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub port: Option<Expression>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub flags: Option<Vec<NatFlag>>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
+pub struct RedirectStatement {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub port: Option<Expression>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub flags: Option<Vec<NatFlag>>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum NatStatement {
+    Snat(SnatStatement),
+    Dnat(DnatStatement),
+    Masquerade(MasqueradeStatement),
+    Redirect(RedirectStatement),
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum RejectType {
+    #[serde(rename = "tcp reset")]
+    TcpReset,
+    Icmpx,
+    Icmp,
+    Icmpv6,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
+pub struct RejectStatement {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "type")]
+    pub rejecttype: Option<RejectType>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub expr: Option<Expression>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum SetOp {
+    Add,
+    Update,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
+pub struct SetStatement {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub op: Option<SetOp>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub elem: Option<Expression>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub set: Option<String>,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
 pub enum Statement {
     Verdict(VerdictStatement),
     Match(MatchStatement),
@@ -152,9 +246,9 @@ pub enum Statement {
     Fwd(FwdStatement),
     Notrack(Option<bool>),
     Dup(DupStatement),
-    Nat,
-    Reject,
-    Set,
+    Nat(NatStatement),
+    Reject(RejectStatement),
+    Set(SetStatement),
     Log,
     CtHelper,
     Meter,
@@ -170,89 +264,106 @@ pub enum Statement {
 mod tests {
     use super::*;
 
-    #[test]
-    fn accept_verdict_serialization() {
-        let v = serde_json::to_string(&VerdictStatement::Accept(None)).unwrap();
-        assert_eq!(v, "{\"accept\":null}");
+    mod verdict {
+        use super::*;
+
+        #[test]
+        fn accept_verdict_serialization() {
+            let v = serde_json::to_string(&VerdictStatement::Accept(None)).unwrap();
+            assert_eq!(v, "{\"accept\":null}");
+        }
+        #[test]
+        fn drop_verdict_serialization() {
+            let v = serde_json::to_string(&VerdictStatement::Drop(None)).unwrap();
+            assert_eq!(v, "{\"drop\":null}");
+        }
+        #[test]
+        fn continue_verdict_serialization() {
+            let v = serde_json::to_string(&VerdictStatement::Continue(None)).unwrap();
+            assert_eq!(v, "{\"continue\":null}");
+        }
+        #[test]
+        fn return_verdict_serialization() {
+            let v = serde_json::to_string(&VerdictStatement::Return(None)).unwrap();
+            assert_eq!(v, "{\"return\":null}");
+        }
+        #[test]
+        fn jump_verdict_serialization() {
+            let v = serde_json::to_string(&VerdictStatement::Jump(JumpVerdict {
+                target: "jump_target".to_string(),
+            }))
+            .unwrap();
+            assert_eq!(v, "{\"jump\":{\"target\":\"jump_target\"}}");
+        }
+        #[test]
+        fn goto_verdict_serialization() {
+            let v = serde_json::to_string(&VerdictStatement::Goto(GotoVerdict {
+                target: "goto_target".to_string(),
+            }))
+            .unwrap();
+            assert_eq!(v, "{\"goto\":{\"target\":\"goto_target\"}}");
+        }
     }
-    #[test]
-    fn drop_verdict_serialization() {
-        let v = serde_json::to_string(&VerdictStatement::Drop(None)).unwrap();
-        assert_eq!(v, "{\"drop\":null}");
+    mod counter {
+        use super::*;
+
+        #[test]
+        fn anonymous_counter_statement_serialization() {
+            let v = serde_json::to_string(&CounterStatement::Anonymous(AnonymousCounter {
+                packets: Some(2),
+                bytes: Some(3),
+            }))
+            .unwrap();
+            assert_eq!(v, "{\"counter\":{\"packets\":2,\"bytes\":3}}");
+        }
+        #[test]
+        fn named_counter_statement_serialization() {
+            let v =
+                serde_json::to_string(&CounterStatement::Named("mycounter".to_string())).unwrap();
+            assert_eq!(v, "{\"counter\":\"mycounter\"}");
+        }
     }
-    #[test]
-    fn continue_verdict_serialization() {
-        let v = serde_json::to_string(&VerdictStatement::Continue(None)).unwrap();
-        assert_eq!(v, "{\"continue\":null}");
+    mod quota {
+        use super::*;
+
+        #[test]
+        fn anonymous_quota_statement_serialization() {
+            let v = serde_json::to_string(&QuotaStatement::Anonymous(AnonymousQuota {
+                val: Some(1),
+                val_unit: Some("kbytes".to_string()),
+                used: Some(0),
+                used_unit: Some("kbytes".to_string()),
+                inv: Some(false),
+            }))
+            .unwrap();
+            assert_eq!(v, "{\"quota\":{\"val\":1,\"val_unit\":\"kbytes\",\"used\":0,\"used_unit\":\"kbytes\",\"inv\":false}}");
+        }
+        #[test]
+        fn named_quota_statement_serialization() {
+            let v = serde_json::to_string(&QuotaStatement::Named("myquota".to_string())).unwrap();
+            assert_eq!(v, "{\"quota\":\"myquota\"}");
+        }
     }
-    #[test]
-    fn return_verdict_serialization() {
-        let v = serde_json::to_string(&VerdictStatement::Return(None)).unwrap();
-        assert_eq!(v, "{\"return\":null}");
-    }
-    #[test]
-    fn jump_verdict_serialization() {
-        let v = serde_json::to_string(&VerdictStatement::Jump(JumpVerdict {
-            target: "jump_target".to_string(),
-        }))
-        .unwrap();
-        assert_eq!(v, "{\"jump\":{\"target\":\"jump_target\"}}");
-    }
-    #[test]
-    fn goto_verdict_serialization() {
-        let v = serde_json::to_string(&VerdictStatement::Goto(GotoVerdict {
-            target: "goto_target".to_string(),
-        }))
-        .unwrap();
-        assert_eq!(v, "{\"goto\":{\"target\":\"goto_target\"}}");
-    }
-    #[test]
-    fn anonymous_counter_statement_serialization() {
-        let v = serde_json::to_string(&CounterStatement::Anonymous(AnonymousCounter {
-            packets: Some(2),
-            bytes: Some(3),
-        }))
-        .unwrap();
-        assert_eq!(v, "{\"counter\":{\"packets\":2,\"bytes\":3}}");
-    }
-    #[test]
-    fn named_counter_statement_serialization() {
-        let v = serde_json::to_string(&CounterStatement::Named("mycounter".to_string())).unwrap();
-        assert_eq!(v, "{\"counter\":\"mycounter\"}");
-    }
-    #[test]
-    fn anonymous_quota_statement_serialization() {
-        let v = serde_json::to_string(&QuotaStatement::Anonymous(AnonymousQuota {
-            val: Some(1),
-            val_unit: Some("kbytes".to_string()),
-            used: Some(0),
-            used_unit: Some("kbytes".to_string()),
-            inv: Some(false),
-        }))
-        .unwrap();
-        assert_eq!(v, "{\"quota\":{\"val\":1,\"val_unit\":\"kbytes\",\"used\":0,\"used_unit\":\"kbytes\",\"inv\":false}}");
-    }
-    #[test]
-    fn named_quota_statement_serialization() {
-        let v = serde_json::to_string(&QuotaStatement::Named("myquota".to_string())).unwrap();
-        assert_eq!(v, "{\"quota\":\"myquota\"}");
-    }
-    #[test]
-    fn anonymous_limit_statement_serialization() {
-        let v = serde_json::to_string(&LimitStatement::Anonymous(AnonymousLimit {
-            rate: Some(1),
-            rate_unit: Some("kbytes".to_string()),
-            per: Some("minutes".to_string()),
-            burst: Some(0),
-            burst_unit: Some("kbytes".to_string()),
-            inv: Some(false),
-        }))
-        .unwrap();
-        assert_eq!(v, "{\"limit\":{\"rate\":1,\"rate_unit\":\"kbytes\",\"per\":\"minutes\",\"burst\":0,\"burst_unit\":\"kbytes\",\"inv\":false}}");
-    }
-    #[test]
-    fn named_limit_statement_serialization() {
-        let v = serde_json::to_string(&LimitStatement::Named("mylimit".to_string())).unwrap();
-        assert_eq!(v, "{\"limit\":\"mylimit\"}");
+    mod limit {
+        use super::*;
+
+        #[test]
+        fn anonymous_limit_statement_serialization() {
+            let v = serde_json::to_string(&LimitStatement::Anonymous(AnonymousLimit {
+                rate: Some(1),
+                rate_unit: Some("kbytes".to_string()),
+                per: Some("minutes".to_string()),
+                burst: Some(0),
+                burst_unit: Some("kbytes".to_string()),
+                inv: Some(false),
+            }))
+            .unwrap();
+            assert_eq!(v, "{\"limit\":{\"rate\":1,\"rate_unit\":\"kbytes\",\"per\":\"minutes\",\"burst\":0,\"burst_unit\":\"kbytes\",\"inv\":false}}");
+        }
+        #[test]
+        fn named_limit_statement_serialization() {
+            let v = serde_json::to_string(&LimitStatement::Named("mylimit".to_string())).unwrap();
+            assert_eq!(v, "{\"limit\":\"mylimit\"}");
+        }
     }
 }
