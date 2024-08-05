4d10f7d7ac9609c6b8a2a820e4162dd29a2b41e7
John Agapeyev <jagapeyev@gmail.com>
2024-08-04 17:42:49 -0700

    Verdict statement boilerplate

--- nftables/src/statement.rs
+++ nftables/src/statement.rs
@@ -1,7 +1,89 @@
 use serde::{Deserialize, Serialize};
 
-//TODO: Continue implementing
+#[derive(Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
+pub struct JumpVerdict {
+    pub target: String,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
+pub struct GotoVerdict {
+    pub target: String,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum VerdictStatement {
+    Accept(Option<bool>),
+    Drop(Option<bool>),
+    Continue(Option<bool>),
+    Return(Option<bool>),
+    Jump(JumpVerdict),
+    Goto(GotoVerdict),
+}
+
 #[derive(Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
 pub enum Statement {
-    TBD,
+    Verdict,
+    Match,
+    Counter,
+    Mangle,
+    Quota,
+    Limit,
+    Fwd,
+    Notrack,
+    Dup,
+    Nat,
+    Reject,
+    Set,
+    Log,
+    CtHelper,
+    Meter,
+    Queue,
+    VerdictMap,
+    CtCount,
+    CtTimeout,
+    CtExpectation,
+    Xt,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn accept_verdict_serialization() {
+        let v = serde_json::to_string(&VerdictStatement::Accept(None)).unwrap();
+        assert_eq!(v, "{\"accept\":null}");
+    }
+    #[test]
+    fn drop_verdict_serialization() {
+        let v = serde_json::to_string(&VerdictStatement::Drop(None)).unwrap();
+        assert_eq!(v, "{\"drop\":null}");
+    }
+    #[test]
+    fn continue_verdict_serialization() {
+        let v = serde_json::to_string(&VerdictStatement::Continue(None)).unwrap();
+        assert_eq!(v, "{\"continue\":null}");
+    }
+    #[test]
+    fn return_verdict_serialization() {
+        let v = serde_json::to_string(&VerdictStatement::Return(None)).unwrap();
+        assert_eq!(v, "{\"return\":null}");
+    }
+    #[test]
+    fn jump_verdict_serialization() {
+        let v = serde_json::to_string(&VerdictStatement::Jump(JumpVerdict {
+            target: "jump_target".to_string(),
+        }))
+        .unwrap();
+        assert_eq!(v, "{\"jump\":{\"target\":\"jump_target\"}}");
+    }
+    #[test]
+    fn goto_verdict_serialization() {
+        let v = serde_json::to_string(&VerdictStatement::Goto(GotoVerdict {
+            target: "goto_target".to_string(),
+        }))
+        .unwrap();
+        assert_eq!(v, "{\"goto\":{\"target\":\"goto_target\"}}");
+    }
 }
